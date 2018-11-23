58a358b0ccc102c3f4617cac68b2510e781dd163
programFairy <1542008040@qq.com>
2018-11-23 19:35:58 +0800

    index final

--- script/index.js
+++ script/index.js
@@ -1,31 +1,109 @@
 $(document).ready(function() {
     //打开页面3s后弹出兴趣选择框
     setTimeout(() => {
-        $('.choose').css('display','block');
+        // $('.choose').css('display', 'block');
     }, 3000);
     //点击关闭按钮关闭兴趣选择框
-    $('.close').click(function(){
-        $('.choose').css('display','none');
-    })
+    $('.close').click(function() {
+        $('.choose').css('display', 'none');
+    });
+    $('.skip').click(function() {
+        $('.choose').css('display', 'none');
+    });
 
-    
+
     //获取选择的城市,并将城市赋值给当前城市
-    $("#submit_city").click(function(){
-        var province = $("#eprovinceName").find("option:selected").text();  
-        var city = $("#ecityName").find("option:selected").text();  
-        $('.city_name').replaceWith(city);
-        $('#distpicker').css('display','none');
+    $("#submit_city").click(function() {
+        var province = $("#eprovinceName").find("option:selected").text();
+        var city = $("#ecityName").find("option:selected").text();
+        $('#cityName').text(city);
+        $('#distpicker').css('display', 'none');
     });
 
     //点击取消选择城市隐藏选择框
-    $('#cancel_city').click(function(){
-        $('#distpicker').css('display','none');
-    })
+    $('#cancel_city').click(function() {
+        $('#distpicker').css('display', 'none');
+    });
 
     //点击切换城市显示城市选择框
-    $('#city_btn').click(function(){
-        $('#distpicker').css('display','block');
+    $('#city_btn').click(function() {
+        $('#distpicker').css('display', 'block');
+    });
+
+    //鼠标滑动到课程显示隐藏的元素内容
+    $('.teacherDes').mouseover(function(event) {
+        var $target = $(event.target);
+        var target_text = $target.text();
+        $target.attr('title', target_text);
+    })
+
+    $('.courseDes').mouseover(function(event) {
+        var $target = $(event.target);
+        var target_text = $target.text();
+        $target.attr('title', target_text);
     })
 
 
+    //鼠标滑至导航显示对应的导航内容
+    $('.left_nav .nav_menu').hover(function() {
+        var nav_index = $(this).index();
+        $('.right_content').find('.menu_content').eq(nav_index-1).css('display','block').siblings('.menu_content').css('display','none');
+    })
+
+    $('.menu_content').mouseleave(function() {
+        $(this).css('display','none');
+        
+    })
+    
+    $('.main_nav').mouseleave(function() {
+        $('.menu_content').css('display','none');
+    })
+
+   
+    //bootstrap手动添加active
+    $('.carousel-inner .item').each(function() {
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        }
+    })
+
+    $('.carousel-indicators li').each(function(index) {
+        if($(this).hasClass('active')){
+            $('.carousel-inner .item').eq(index).addClass('active').siblings().removeClass('active');
+        }
+    })
+   
+   //点击喜欢变成红色桃心
+   $('.like .collect').click(function() {     
+       if($(this).hasClass('fa-heart')){
+        $(this).removeClass('fa-heart').addClass('fa-heart-o').css('color','#000');
+       }else{
+        $(this).removeClass('fa-heart-o').addClass('fa-heart').css('color','red');
+       }
+   })
+
+    //点击购物车动画效果  rotateOutUpRight
+    $('.like i:nth-child(2)').click(function() {
+        $(this).addClass('animated rollOut');
+        setTimeout(function(){
+            $('.like i:nth-child(2)').removeClass('rollOut');
+        }, 1000);
+    })
+ 
+
+    //购物车数量
+    if( $('.cartlist').css('display') == 'block'){
+        var cartnum = $('.cartlist ul li').length;
+        $('.shopcart').find('.total').text(cartnum);
+    }else{
+        $('.shopcart').find('.total').text('0');
+    }
+   
+    //鼠标滑动至购物车显示对应的框
+    $('.header_nav li:nth-child(3)').hover(function(){
+        $('.shopcart').css('display','block')
+    })
+    $('.shopcart').mouseleave(function(){
+        $(this).css('display','none')
+    })
 })
--- script/login.js
+++ script/login.js
@@ -1,12 +1,12 @@
 $(function(){
-    //点击标签切换对应的模块
+    //点击标签切换对应登录方式的模块
     var userLogin = document.getElementById('userLogin');
     var phoneLogin = document.getElementById('phoneLogin');
     var underline = document.getElementById('underline');
     var userWay = document.getElementById('user_way');
     var phoneWay = document.getElementById('phone_way');
     userLogin.onclick = function(){
-        userLogin.style.color = '#35AE35';
+        userLogin.style.color = '#FFC125';
         phoneLogin.style.color = '#000';
         underline.style.marginLeft = '0'; 
         userWay.style.display = 'block';
@@ -15,21 +15,82 @@ $(function(){
     }
     phoneLogin.onclick = function(){
         userLogin.style.color = '#000';
-        phoneLogin.style.color = '#35AE35';
+        phoneLogin.style.color = '#FFC125';
         underline.style.marginLeft = '205px';
         userWay.style.display = 'none';
         phoneWay.style.display = 'block';
         document.getElementById('forgotTxt').innerHTML = '';
     }
-    //点击获取验证码后倒计时
+
+
+
+    //表单验证
+    var name_psw =  new RegExp(/^[A-Z0-9a-z]{6,12}$/);
+
+    $('input[name="username"]').blur(function() {
+        var $name = $('input[name="username"]');
+        var name_val = $.trim($name.val());
+        var name_error = $('#name_error');
+         //验证用户名
+         if(name_val == ""){
+            name_error.text('用户名不能为空');
+            $name.focus();
+            $name.next().addClass('errorIco');
+            return;
+        }else if(name_psw.test(name_val)){
+            name_error.text('');
+            $name.next().removeClass('errorIco');
+        }else{
+            name_error.text('用户名必须为6-12位数字或字母');
+            $name.focus();
+            $name.next().addClass('errorIco');
+            return;
+        };
+    });
+    $('input[name="password"]').blur(function() {
+        var $psw = $('input[name="password"]');     
+        var psw_val = $.trim($psw.val());
+        var psw_error = $('#psw_error');
+        //验证密码；
+        if(psw_val == ""){
+            psw_error.text('密码不能为空');
+            $psw.next().addClass('errorIco');
+            // $psw.focus();
+            // return;
+        }else if(name_psw.test(psw_val)){
+            psw_error.text('');
+            $psw.next().removeClass('errorIco');
+        }else{
+            psw_error.text('密码必须为6-12位数字或字母');
+            $psw.next().addClass('errorIco');
+            // $psw.focus();
+            // return;
+        };
+    });
+
+    //手机号
+    var $phone= $('input[name="phoneNumber"]');
+    var phone_val = $.trim($phone.val());
+    var phone_error = $('#phone_error');
+    // 验证手机正则规则
+    var phone_isvalid =  new RegExp(/^(13[0-9]|15[012356789]|18[0-9]|17[678]|14[57])[0-9]{8}$/);
+    //倒计时
     var btn = document.getElementById('btn');
     var clock = '';	
     var nums = 60;	
     var btn;
     btn.onclick = function (){	
-        btn.style.disabled = true; //将按钮置为不可点击		
-        btn.value = nums +'秒后可重新获取';		
-        clock = setInterval(doLoop, 1000); //一秒执行一次	
+        if($('input[name="phoneNumber"]').val() != '' && phone_isvalid.test(phone_val)){
+            btn.style.disabled = true; //将按钮置为不可点击		
+            btn.value = nums +'秒后可重新获取';		
+            clock = setInterval(doLoop, 1000); //一秒执行一次
+        }else{
+            btn.style.disabled = true;
+            phone_error.text('请输入正确的手机号');
+            $phone.siblings('span').addClass('errorIco');
+            $phone.focus();
+            return;
+        }	
     }
 
     function doLoop(){		
@@ -44,62 +105,25 @@ $(function(){
             }	
     }
 
-    //表单验证
-    var name_psw =  new RegExp(/^[A-Z0-9a-z]{6,12}$/);
-
-    $('input[name="username"]').blur(function() {
-        var $name = $('input[name="username"]');
-        var name_val = $.trim($name.val());
-        var name_error = $('#name_error');
-         //验证用户名
-         if(name_val == ""){
-            name_error.text('*用户名不能为空');
-            $name.focus();
-            return;
-        }else if(name_psw.test(name_val)){
-            name_error.text('');
-        }else{
-            name_error.text('*用户名必须为6-12位数字或字母');
-            $name.focus();
-            return;
-        };
-    });
-    $('input[name="password"]').blur(function() {
-        var $psw = $('input[name="password"]');     
-        var psw_val = $.trim($psw.val());
-        var psw_error = $('#psw_error');
-        //验证密码；
-        if(psw_val == ""){
-            psw_error.text('*密码不能为空');
-            $psw.focus();
-            return;
-        }else if(name_psw.test(psw_val)){
-            psw_error.text('');
-        }else{
-            psw_error.text('*密码必须为6-12位数字或字母');
-            $psw.focus();
-            return;
-        };
-    });
-
     $('input[name="phoneNumber"]').blur(function() {
-        var $phone= $('input[name="phoneNumber"]');
-        var phone_val = $.trim($phone.val());
-        var phone_error = $('#phone_error');
-        // 验证手机
-        var phone_isvalid =  new RegExp(/^(13[0-9]|15[012356789]|18[0-9]|17[678]|14[57])[0-9]{8}$/);
         if(phone_val == ""){
-            phone_error.text('*手机号不能为空');
+            phone_error.text('手机号不能为空');
+            $phone.siblings('span').addClass('errorIco');
             $phone.focus();
             return;
         }else if(phone_isvalid.test(phone_val)){
             phone_error.text('');
+            $phone.next().removeClass('errorIco');
         }else{
-            phone_error.text('*手机号格式不正确');
+            phone_error.text('手机号格式不正确');
+            $phone.next().addClass('errorIco');
             $phone.focus();
             return;
         };
     });
+
+    
+
     $('input[name="phoneCode"]').blur(function() {
         var $code= $('input[name="phoneCode"]'); 
         var code_val = $.trim($code.val());
@@ -107,14 +131,17 @@ $(function(){
         var vercode = new RegExp(/^[0-9]{6}$/);  
         //验证码
         if(code_val == ""){
-            verify_error.text('*验证码不能为空');
-            $code.focus();
+            verify_error.text('验证码不能为空');
+            $code.next().addClass('errorIco');
+            // $code.focus();
             return;
         }else if(vercode.test(code_val)){
             verify_error.text('');
+            $code.next().addClass('errorIco');
         }else{
-            verify_error.text('*验证码错误');
-            $code.focus();
+            verify_error.text('验证码错误');
+            $code.next().addClass('errorIco');
+            // $code.focus();
             return;
         }
     })
